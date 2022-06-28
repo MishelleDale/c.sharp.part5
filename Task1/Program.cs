5b22b0bcc17261bcdc3c43059749a985c87de5a8
Ekaterina <mishelledale@gmail.com>
2022-06-28 17:02:38 +0300

    Task1: Let the user print some numbers and count the quantity of positive ones is done

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Сколько цифр вы хотите ввести?");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int[] Quantity = new int[Number];
+
+int[] FillArray (int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число");
+        int Digit = Convert.ToInt32(Console.ReadLine());
+        array[i] = Digit;
+       // Console.Write(Digit);
+    }
+    return array;
+} 
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void CountPositive (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            sum = sum + 1;
+        }
+    }
+    Console.WriteLine("Вы ввели " + sum + " чисел(ла) больше 0");
+}
+
+int[] arr = FillArray(Quantity);
+PrintArray(arr);
+Console.WriteLine();
+CountPositive(arr);
