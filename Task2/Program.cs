b10b89c26386a4d822c25448907c754d81c31184
Ekaterina <mishelledale@gmail.com>
2022-06-28 17:32:09 +0300

    Task2: Convert the number to 01 system is done

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number <= 1 && Number >= 0) Console.WriteLine(Number);
+if (Number == -1) Console.WriteLine($"{1}");
+
+if (Number > 1)
+{
+    string Number2 = Convert.ToString(Number, 2);
+    Console.WriteLine("Введенное вами число в двоичной системе: " + Number2);
+}
+
+if (Number < -1)
+{
+    int NumberPos = Number * (-1);
+    string Number2 = Convert.ToString(NumberPos, 2);
+    Console.WriteLine("Введенное вами число в двоичной системе: " + Number2);
+}
